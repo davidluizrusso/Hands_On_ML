6bfa8cbb2b31a65314e7d6c15a276627aa03cb31
davidluizrusso <david.luiz.russo@gmail.com>
2017-07-25 20:38:21 -0700

    Added input and output directories, data, and more exercises

--- Chapter3/ch3_exercises.py
+++ Chapter3/ch3_exercises.py
@@ -1,9 +1,3 @@
-############################################# Exercise 1 #############################################
-
-# Build a classifier for the MNIST dataset that achieves over 97% accuracy on the test set.
-# Hint: KNeighborsClassifier works well, just need to try a grid search on the weights and n_neighbors
-# hyperparameters. 
-
 # load libraries
 from sklearn.datasets import fetch_mldata
 from sklearn.model_selection import cross_val_score
@@ -13,6 +7,19 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.ndimage.interpolation import shift
+import csv
+import pandas as pd
+import os
+
+
+############################################# Exercise 1 #############################################
+
+# Build a classifier for the MNIST dataset that achieves over 97% accuracy on the test set.
+# Hint: KNeighborsClassifier works well, just need to try a grid search on the weights and n_neighbors
+# hyperparameters. 
 
 # import MNIST data
 mnist = fetch_mldata('MNIST original')
@@ -57,13 +64,47 @@ conf_best = confusion_matrix(y_test, best_preds)
 
 ############################################# Exercise 2 #############################################
 
+# Write a function that can shift an MNIST image one unit in any direction. 
+
+def Shift_Image(im, x_shift, y_shift):
+    return shift(im.reshape(28, 28), [x_shift, y_shift], cval = 0).reshape(784)
+    
+Shift_Image(X[1], 2, 1)
+X[1]
 
 
+# Write function to plot images 
+plt.imshow(X[36000].reshape(28, 28),
+           cmap = matplotlib.cm.binary,
+           interpolation = "nearest")
+           
+# Write function to plot images 
+plt.imshow(Shift_Image(X[36000], 5, 5).reshape(28, 28),
+           cmap = matplotlib.cm.binary,
+           interpolation = "nearest")
+           
+X_expanded = [X]
+
+for x_shift, y_shift in (-1, 0), (1, 0), (0, -1), (0, 1):
+    shifted_images = np.apply_along_axis(func1d = Shift_Image,
+                                         axis = 1,
+                                         arr = X,
+                                         x_shift = x_shift,
+                                         y_shift = y_shift)
+    X_expanded.append(shifted_images)
+
+[item.shape for item in X_expanded]    
+X_expanded = np.concatenate(X_expanded) # collapse list into single ndarray
+X_expanded.shape
 
 ############################################# Exercise 3 #############################################
 
+# load training and testing data for Titanic data set
+INPUT_DIR = os.path.join("/Users/davidrusso/Documents/Programming/Python/Hands_On_ML/Chapter3", "Input")
+OUTPUT_DIR = os.path.join("/Users/davidrusso/Documents/Programming/Python/Hands_On_ML/Chapter3", "Output")
 
 
+      
 
 ############################################# Exercise 4 #############################################
 
