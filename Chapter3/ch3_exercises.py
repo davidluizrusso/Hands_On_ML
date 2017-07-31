ab6627a2dc18c044b9175095f5feb38495cd2ed1
davidluizrusso <david.luiz.russo@gmail.com>
2017-07-30 19:28:38 -0700

    Added part of exercise 4 solution

--- Chapter3/ch3_exercises.py
+++ Chapter3/ch3_exercises.py
@@ -176,7 +176,20 @@ importance = pd.DataFrame(importance, index=trainX.columns,
                           columns=["Importance"])
 
 ############################################# Exercise 4 #############################################
+### build a spam classifier 
+
+# 1 - Download examples of spam and ham and load into a data frame
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]    
+    all_words = []       
+    for mail in emails:    
+        with open(mail) as m:
+            for i,line in enumerate(m):
+                if i == 2:  #Body of email is only 3rd line of text file
+                    words = line.split()
+                    all_words += words
+    dictionary = Counter(all_words)
 
 
 
-############################################# Exercise 5 #############################################
+
