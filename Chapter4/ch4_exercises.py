2ed9769ad68a409d7cc6b80ce948c184aa2dbdd0
davidluizrusso <david.luiz.russo@gmail.com>
2017-08-20 14:51:37 -0700

    Finished all but one exercise

--- Chapter4/ch4_exercises.py
+++ Chapter4/ch4_exercises.py
@@ -89,6 +89,80 @@
 #    learning rate. 
 
 
+### ### ### ### ###
+### Question 8 ###
+### ### ### ### ### 
+
+# Question 
+#    Suppose you are using polynomial regression. You plot the learning curves and you notice that
+#    there is a large gap between the training error and the validation error. What is happening? 
+#    What are three ways to solve this? 
+
+# Answer 
+#    You may be overfitting the training data. 
+#    1) You could reduce the polynomial degree that you are using. 
+#    2) You could regularize the model via LASSO or Ridge or Elastic net. 
+#    3) You can increase the size of the training set. 
+
+### ### ### ### ###
+### Question 9 ###
+### ### ### ### ### 
+
+# Question
+#    Suppose you are using Ridge Regression and you notice the training error and validation error 
+#    are about equal and fairly high. Does your model suffer from high bias or high variance? 
+#    Should you increase the regularization hyperparameter (alpha) or reduce it? 
+
+# Answer
+#    Your model suffers from high bias. It is not capable of capturing the true relationship in its 
+#    current form. You should reduce the hyperparameter alpha. 
+
+
+### ### ### ### ###
+### Question 10 ###
+### ### ### ### ### 
+
+# Question
+#    Why would you want to use:
+#    1) Ridge Regression over Linear Regression?
+#    2) Lasso instead of Ridge Regression?
+#    3) Elastic net instead of Lasso? 
+
+# Answer
+#    1) When you want to constrain the parameters in some way to avoid overfitting. Regularized
+#       models tend to perform better than unregularized models.
+#
+#    2) Lasso is preferable to ridge regression if you want to perform feature selection in an 
+#       automated fashion. If you suspect that only a few features are important, LASSO may be 
+#       a better option than Ridge. Otherwise, Ridge is preferable. 
+#
+#    3) Elastic net is preferable over Lasso because Lasso tends to remove correlated features at 
+#       random, which can lead to erratic behavior. Elastic net does require an extra parameter to train.
+
+
+### ### ### ### ###
+### Question 11 ###
+### ### ### ### ### 
+
+# Question
+#    Suppose you want to classify pictures as outdoor/indoor and daytime/nighttime. 
+#    Should you implement two LR classifiers or one Softmax regression classifier? 
+#
+# Answer
+#    You should train two LR classifiers if you want the outputs to be distinctly outdoor/indoor
+#    and daytime/night time. If you can live with classifiying outdoor pictures as outdoor/daytime
+#    and outdoor/nighttime and indoor pictures are indoor/daytime and indoor/nighttime then you
+#    could use a softmax classifier with the four aforementioned classes. 
+
+### ### ### ### ###
+### Question 12 ###
+### ### ### ### ### 
+
+# Question
+#    Implement batch gradient descent with early stopping for softmax regression (without using
+#    scikit-learn).
+
+# Answer
 
 
 
