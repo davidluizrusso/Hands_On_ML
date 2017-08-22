d62ff803c755ecc7f29369b1be80e7b22ac13a41
davidluizrusso <david.luiz.russo@gmail.com>
2017-08-21 21:43:33 -0700

    Finished ch 4 exercises

--- Chapter4/ch4_exercises.py
+++ Chapter4/ch4_exercises.py
@@ -1,5 +1,21 @@
 # Exercises for Chapter 4
 
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.base import clone
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
 ### ### ### ### ###
 ### Question 1 ###
 ### ### ### ### ### 
@@ -164,6 +180,84 @@
 
 # Answer
 
+# set random number seed for repro
+np.random.seed(2042)
+
+# load data
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]
+y = iris["target"]
+
+# add bias term to X
+X_with_bias = np.c_[np.ones([len(X), 1]), X]
+
+# create training, validation, and test sets without sklearn
+test_ratio = 0.2
+validation_ratio = 0.2
+total_size = len(X_with_bias)
+
+test_size = int(total_size * test_ratio)
+validation_size = int(total_size * validation_ratio)
+train_size = total_size - test_size - validation_size
+
+rnd_indices = np.random.permutation(total_size)
+
+X_train = X_with_bias[rnd_indices[:train_size]]
+y_train = y[rnd_indices[:train_size]]
+X_valid = X_with_bias[rnd_indices[train_size:-test_size]]
+y_valid = y[rnd_indices[train_size:-test_size]]
+X_test = X_with_bias[rnd_indices[-test_size:]]
+y_test = y[rnd_indices[-test_size:]]
 
 
+def to_one_hot(y):
+    n_classes = y.max() + 1
+    m = len(y)
+    Y_one_hot = np.zeros((m, n_classes))
+    Y_one_hot[np.arange(m), y] = 1
+    return Y_one_hot
+    
+Y_train_one_hot = to_one_hot(y_train)
+Y_valid_one_hot = to_one_hot(y_valid)
+Y_test_one_hot = to_one_hot(y_test)
 
+def softmax(logits):
+    exps = np.exp(logits)
+    exp_sums = np.sum(exps, axis=1, keepdims=True)
+    return exps / exp_sums    
+
+
+n_inputs = X_train.shape[1] # two features + one bias term 
+n_outputs = len(np.unique(y_train))  # three distinct classes to predict
+
+eta = 0.01 # learning rate
+n_iterations = 5001 
+m = len(X_train) 
+epsilon = 1e-7 # acceptance threshold 
+best_loss = np.infty
+
+Theta = np.random.randn(n_inputs, n_outputs)
+
+for iteration in range(n_iterations):
+    logits = X_train.dot(Theta) # scores
+    Y_proba = softmax(logits) # 
+    loss = -np.mean(np.sum(Y_train_one_hot * np.log(Y_proba + epsilon), axis=1))
+    error = Y_proba - Y_train_one_hot
+    if iteration % 500 == 0:
+        print(iteration, loss)
+    if loss < best_loss:
+        best_loss = loss
+    else:
+        print(iteration - 1, best_loss)
+        print(iteration, loss, "early stopping!")
+        break
+    gradients = 1/m * X_train.T.dot(error)
+    Theta = Theta - eta * gradients
+    
+    
+logits = X_valid.dot(Theta)
+Y_proba = softmax(logits)
+y_predict = np.argmax(Y_proba, axis=1)
+
+accuracy_score = np.mean(y_predict == y_valid)
+accuracy_score
